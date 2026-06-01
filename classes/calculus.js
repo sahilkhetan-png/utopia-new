a27c79ce04e46ffcf6c7eabd281f29e61e633b1e
sahilkhetan-png <sahil.khetan@lps-students.org>
2026-06-01 07:12:15 -0400

    Create calculus.js

---
+++ classes/calculus.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/math/',
+    bare: '/book/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/classes/history.js',
+    bundle: '/classes/balance.js',
+    config: '/classes/calculus.js',
+    sw: '/classes/science.js',
+};
